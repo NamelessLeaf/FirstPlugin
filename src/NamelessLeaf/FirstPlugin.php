28ffba2d289312d3268c96aa5421c28ffe27ba8d
NamelessLeaf <74725613+NamelessLeaf@users.noreply.github.com>
2020-12-10 19:21:51 +0000

    Update FirstPlugin.php

--- src/NamelessLeaf/FirstPlugin.php
+++ src/NamelessLeaf/FirstPlugin.php
@@ -5,6 +5,27 @@ namespace NamelessLeaf;
 use pocketmine\Player;
 use pocketmine\Server;
      
-use pocketmine\plugin\{PluginBase};
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;      
+use pocketmine\command\CommandSender;
+      
+use pocketmine\event\EventListener;
+use pocketmine\player\PlayerJoinEvent;
 
+use pocketmine\utils\TextFormat;
+
+class Main extends pluginbase implements Listener {
+      
+      public Function onEnable() {
+          $this->getServer()->getPluginManager()->registerEvents($this , $this)
+      }
+          
+          }      
+      
+      
+      
+      
+      
+      
+      
       
